c4a1c7d970edd0ce2b22ed820f78def646da3a8d
devidesai2011 <devidesai2011@gmail.com>
2025-07-27 11:59:55 +0530

    React: Day 2

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+/**
+ * <div id="parent">
+ *    <div id="child">
+ *      <h1 id="heading">Hello world from React!</h1>
+ *    </div>
+ * </div>
+ * 
+ */
+
+const parent = React.createElement(
+    "div",
+    {id: 'parent'},
+    [React.createElement("div", {id: 'child'}, [React.createElement("h1", null, "I am h1 tag!"),React.createElement("h2", null, "I am h2 tag!")]),
+        React.createElement("div", {id: 'child2'}, [React.createElement("h1", null, "I am h1 tag!"),React.createElement("h2", null, "I am h2 tag!")])]
+    );
+
 const heading = React.createElement(
     'h1',
     {id: 'heading'},
@@ -6,4 +22,4 @@ const heading = React.createElement(
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading)
+root.render(parent)
